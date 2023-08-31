86b118dba4f20cafe7e37571204937dc94c0dc3d
Pouriya Jamshidi <54482226+pouriyajamshidi@users.noreply.github.com>
2023-08-31 19:27:33 +0200

    fix license

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+//
+// Copyright (c) 2022-2023 Pouriya Jamshidi (pouriya at thegraynode dot io)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Official repository: https://github.com/pouriyajamshidi/oxipot
+//
+
 use chrono::{DateTime, Utc};
 use env_logger;
 use log::{error, info, warn};
