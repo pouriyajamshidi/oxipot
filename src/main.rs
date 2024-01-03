3fed53cc7e8a2042cbc6f59f45723210b0ac2766
Pouriya Jamshidi <54482226+pouriyajamshidi@users.noreply.github.com>
2024-01-03 22:41:25 +0100

    update license

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2022-2023 Pouriya Jamshidi (pouriya at thegraynode dot io)
+// Copyright (c) 2022-2024 Pouriya Jamshidi (pouriya at thegraynode dot io)
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
