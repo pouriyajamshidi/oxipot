32cb4c2ab73d9859f3a6b515b4e08b96d53033bc
Pouriya Jamshidi <54482226+pouriyajamshidi@users.noreply.github.com>
2024-09-01 20:02:12 +0200

    temporary remove static ssl and crypto directives

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
-    println!("cargo:rustc-link-lib=static=ssl");
-    println!("cargo:rustc-link-lib=static=crypto");
+    // println!("cargo:rustc-link-lib=static=ssl");
+    // println!("cargo:rustc-link-lib=static=crypto");
 
     println!("cargo:rustc-cfg=release");
     println!("cargo:rustc-env=RUSTFLAGS=-C optimization=3 -C lto");
