ea9fde48b4cc794897db6a5e1d1ec24f8caded32
pouriyajamshidi <pouriya.jamshidi@gmail.com>
2024-09-01 18:54:21 +0200

    update dependencies and remove unneeded build flag

--- build.rs
+++ build.rs
@@ -1,9 +1,7 @@
 fn main() {
-    #[cfg(feature = "static")]
-    {
-        println!("cargo:rustc-link-lib=static=ssl");
-        println!("cargo:rustc-link-lib=static=crypto");
-    }
+    println!("cargo:rustc-link-lib=static=ssl");
+    println!("cargo:rustc-link-lib=static=crypto");
+
     println!("cargo:rustc-cfg=release");
     println!("cargo:rustc-env=RUSTFLAGS=-C optimization=3 -C lto");
     println!("cargo:rustc-cfg=static");
